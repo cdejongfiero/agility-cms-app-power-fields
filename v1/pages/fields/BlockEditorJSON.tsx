31841ed9d4af1095f51945593defaf816fd01b3e
Dakotah Godfrey <dakotah.godfrey.dev@gmail.com>
2024-11-19 16:03:59 -0500

    Dynamic Imports

--- v1/pages/fields/BlockEditorJSON.tsx
+++ v1/pages/fields/BlockEditorJSON.tsx
@@ -1,14 +1,9 @@
-
-
 import { useAgilityAppSDK } from "@agility/app-sdk"
 import dynamic from "next/dynamic"
 
 const BlockEditor = dynamic(() => import("../../components/BlockEditor"), { ssr: false })
-
 export default function BlockEditorPage() {
-
 	const { initializing, appInstallContext } = useAgilityAppSDK()
 	if (initializing) return null
 	return <BlockEditor configuration={appInstallContext?.configuration} />
-
 }
