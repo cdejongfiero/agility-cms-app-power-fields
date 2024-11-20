c54e287334248d5ce7a0d77399dde78742ea2a58
Dakotah Godfrey <dakotah.godfrey.dev@gmail.com>
2024-11-20 11:20:14 -0500

    Tweak to have set initial button text

--- v1/components/FriendlySlugField.tsx
+++ v1/components/FriendlySlugField.tsx
@@ -1,11 +1,12 @@
-import { useAgilityAppSDK, contentItemMethods, setHeight, openAlertModal, setFocus } from "@agility/app-sdk"
-import { useEffect, useState } from "react"
-import { FormInputWithAddons, INestedInputButtonProps, UnifiedIconName } from "@agility/plenum-ui"
-import { FOCUS_EVENTS, handleFieldFocusEvent } from "@/methods/handleFieldFocusEvent"
+import { useAgilityAppSDK, contentItemMethods, setHeight, openAlertModal, setFocus } from "@agility/app-sdk";
+import { useEffect, useState } from "react";
+import { FormInputWithAddons, INestedInputButtonProps, UnifiedIconName } from "@agility/plenum-ui";
+import { FOCUS_EVENTS, handleFieldFocusEvent } from "@/methods/handleFieldFocusEvent";
+import { tablerIconNames } from "@agility/plenum-ui/stories/atoms/icons/tablerIconNames";
 
 const makeFriendlyStr = (s: string): string => {
-	if (!s) return ""
-	let friendly = s.toLowerCase()
+	if (!s) return "";
+	let friendly = s.toLowerCase();
 	friendly = friendly
 		.replace(new RegExp("\\s", "g"), "-")
 		.replace(new RegExp("[àáâãäå]", "g"), "a")
@@ -19,91 +20,95 @@ const makeFriendlyStr = (s: string): string => {
 		.replace(new RegExp("[ùúûü]", "g"), "u")
 		.replace(new RegExp("[ýÿ]", "g"), "y")
 		.replace(new RegExp("[^\\w\\-@-]", "g"), "-")
-		.replace(new RegExp("--+", "g"), "-")
+		.replace(new RegExp("--+", "g"), "-");
 
 	if (friendly.lastIndexOf("-") > 0 && friendly.lastIndexOf("-") == friendly.length - 1) {
-		friendly = friendly.substring(0, friendly.length - 1)
+		friendly = friendly.substring(0, friendly.length - 1);
 	}
-	return friendly
-}
+	return friendly;
+};
 
 const FriendlyURLField = () => {
-	const { contentItem, field, fieldValue } = useAgilityAppSDK()
+	const { contentItem, field, fieldValue } = useAgilityAppSDK();
 
-	const [currentTitle, setCurrentTitle] = useState<string>("")
-	const [CTAIcon, setCTAIcon] = useState<UnifiedIconName | undefined>(undefined)
-	const [CTALabel, setCTALabel] = useState<string | undefined>(undefined)
-	const [width, setWidth] = useState<number>(document.body.clientWidth)
+	const [currentTitle, setCurrentTitle] = useState<string>("");
+	const [CTAIcon, setCTAIcon] = useState<UnifiedIconName | undefined>(undefined);
+	const [CTALabel, setCTALabel] = useState<string | undefined>("Re-Generate Slug");
+	const [width, setWidth] = useState<number>(document.body.clientWidth);
 
 	const regenerateSlug = async (title: string) => {
-		const newVal = makeFriendlyStr(title)
-		contentItemMethods.setFieldValue({ name: field?.name, value: newVal })
-	}
-	const hasBeenSaved = !!!(contentItem && contentItem?.contentID < 0)
+		const newVal = makeFriendlyStr(title);
+		contentItemMethods.setFieldValue({ name: field?.name, value: newVal });
+	};
+	const hasBeenSaved = !!!(contentItem && contentItem?.contentID < 0);
 
 	const intializeTitleFieldListener = () => {
 		contentItemMethods.addFieldListener({
 			fieldName: "Title",
 			onChange: (t) => {
-				setCurrentTitle(t)
+				setCurrentTitle(t);
 				contentItemMethods?.getContentItem()?.then((ci) => {
 					//only regenerate the slug if the content item is new
-					const isNewContentItem = Boolean((ci?.contentID ?? -1) < 0)
+					const isNewContentItem = Boolean((ci?.contentID ?? -1) < 0);
 					if (isNewContentItem) {
-						regenerateSlug(t)
+						regenerateSlug(t);
 					}
-				})
+				});
 			}
-		})
-	}
+		});
+	};
 
 	const handleResizeListener: (isCleanup?: boolean) => void = (isCleanup) => {
-		const handleResize = () => setWidth(document.body.clientWidth)
+		const handleResize = () => setWidth(document.body.clientWidth);
 		if (isCleanup) {
-			window.removeEventListener("resize", handleResize)
-			return
+			window.removeEventListener("resize", handleResize);
+			return;
 		} else {
-			window.addEventListener("resize", handleResize)
-			return
+			window.addEventListener("resize", handleResize);
+			return;
 		}
-	}
+	};
 
 	useEffect(() => {
-		setHeight({ height: 50 })
-		intializeTitleFieldListener()
-		handleResizeListener()
+		setHeight({ height: 50 });
+		intializeTitleFieldListener();
+		handleResizeListener();
 		return () => {
-			handleResizeListener(true)
-		}
-	}, [])
+			handleResizeListener(true);
+		};
+	}, []);
 
 	useEffect(() => {
 		if (hasBeenSaved) {
-			setCTAIcon("RefreshIcon")
-			setCTALabel("Re-Generate Slug")
+			setCTAIcon("RefreshIcon");
+			setCTALabel("Re-Generate Slug");
 			if (width < 400) {
-				setCTALabel(undefined)
+				setCTALabel(undefined);
 			}
 		} else {
-			setCTAIcon(undefined)
-			setCTALabel(undefined)
+			setCTAIcon(undefined);
+			setCTALabel(undefined);
 		}
-	}, [hasBeenSaved, width])
+	}, [hasBeenSaved, width]);
 
 	const handleCTAClick = () => {
-		openAlertModal({
-			title: "Re-Generate Slug",
-			message:
-				"By changing the URL you could create broken links. We recommend you add in a URL redirection from the old URL to the new URL. Are you sure you wish to continue?",
-			okButtonText: "Re-Generate",
-			cancelButtonText: "Cancel",
-			iconName: "QuestionMarkCircleIcon",
-			callback: (ok: boolean) => {
-				if (!ok) return
-				regenerateSlug(currentTitle)
-			}
-		})
-	}
+		if (!hasBeenSaved) {
+			return;
+		} else {
+			openAlertModal({
+				title: "Re-Generate Slug",
+				message:
+					"By changing the URL you could create broken links. We recommend you add in a URL redirection from the old URL to the new URL. Are you sure you wish to continue?",
+				okButtonText: "Re-Generate",
+				cancelButtonText: "Cancel",
+				iconName: "QuestionMarkCircleIcon",
+				callback: (ok: boolean) => {
+					if (!ok) return;
+					regenerateSlug(currentTitle);
+				}
+			});
+		}
+	};
 
 	const formInputBTNProps: INestedInputButtonProps = {
 		onClick: handleCTAClick,
@@ -114,12 +119,13 @@ const FriendlyURLField = () => {
 		},
 		align: "right",
 		onFocus: () => {
-			handleFieldFocusEvent({ eventName: FOCUS_EVENTS.FOCUS })
+			handleFieldFocusEvent({ eventName: FOCUS_EVENTS.FOCUS });
 		},
 		onBlur: () => {
-			handleFieldFocusEvent({ eventName: FOCUS_EVENTS.BLUR })
-		}
-	}
+			handleFieldFocusEvent({ eventName: FOCUS_EVENTS.BLUR });
+		},
+		disabled: !hasBeenSaved
+	};
 	return (
 		<div className="flex flex-row items-center justify-between gap-1">
 			<div className="w-full p-1 ">
@@ -128,19 +134,19 @@ const FriendlyURLField = () => {
 					value={fieldValue}
 					name={field?.name || ""}
 					handleChange={(value: string) => {
-						regenerateSlug(value)
+						regenerateSlug(value);
 					}}
 					onFocus={() => {
-						handleFieldFocusEvent({ eventName: FOCUS_EVENTS.FOCUS })
+						handleFieldFocusEvent({ eventName: FOCUS_EVENTS.FOCUS });
 					}}
 					onBlur={() => {
-						handleFieldFocusEvent({ eventName: FOCUS_EVENTS.BLUR })
+						handleFieldFocusEvent({ eventName: FOCUS_EVENTS.BLUR });
 					}}
 					addonBTN={formInputBTNProps}
 				/>
 			</div>
 		</div>
-	)
-}
+	);
+};
 
-export default FriendlyURLField
+export default FriendlyURLField;
